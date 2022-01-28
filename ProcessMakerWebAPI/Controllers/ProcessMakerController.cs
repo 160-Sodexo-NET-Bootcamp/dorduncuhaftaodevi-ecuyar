c7681c9471eb2ec121b4ef8cf8cbe0014f452f25
Enes Can UYAR <uyar.enescan@gmail.com>
2022-01-28 17:55:06 +0300

    Initial project and Hangfire

---
+++ ProcessMakerWebAPI/Controllers/ProcessMakerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProcessMakerWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProcessMakerController : ControllerBase
+    {
+        public ProcessMakerController()
+        {
+
+        }
+    }
+}

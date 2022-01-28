27f4e96070ca2b1038aeb8ad69e6a74f33dc6175
Enes Can UYAR <uyar.enescan@gmail.com>
2022-01-28 19:48:53 +0300

    Add generic repository

---
+++ ProcessMaker_Data/Repositories/GenericRepository.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProcessMaker_Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessMaker_Data.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected ProcessMakerDbContext context;
+        internal DbSet<T> dbSet;
+
+        public GenericRepository(ProcessMakerDbContext context)
+        {
+            this.context = context;
+
+            dbSet = context.Set<T>();
+        }
+
+        public virtual async Task<bool> Add(T entity)
+        {
+            await dbSet.AddAsync(entity);
+            return true;
+        }
+
+        public bool Delete(int id)
+        {
+            var model = dbSet.Find(id);
+
+            if (model == null)
+            {
+                return false;
+            }
+
+            dbSet.Remove(model);
+            return true;
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAll()
+        {
+            return await dbSet.ToListAsync();
+        }
+
+        public virtual async Task<T> GetById(int id)
+        {
+            var model = await dbSet.FindAsync(id);
+            return model;
+        }
+
+        public bool Update(T entity)
+        {
+            if (entity != null)
+            {
+                dbSet.Update(entity);
+                return true;
+            }
+            return false;
+        }
+
+        public virtual async Task<IEnumerable<T>> Where(Expression<Func<T, bool>> predicate)
+        {
+            return await dbSet.Where(predicate).ToListAsync();
+        }
+    }
+}
---
+++ ProcessMaker_Data/Repositories/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessMaker_Data.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<bool> Add(T entity);
+        bool Update(T entity);
+        Task<T> GetById(int id);
+        Task<IEnumerable<T>> GetAll();
+        bool Delete(int id);
+        Task<IEnumerable<T>> Where(Expression<Func<T, bool>> predicate);
+    }
+}

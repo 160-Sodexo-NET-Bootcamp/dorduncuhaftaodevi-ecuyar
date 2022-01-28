9135543ff980c7d8e214a723394267d412fcbc73
Enes Can UYAR <uyar.enescan@gmail.com>
2022-01-28 20:01:03 +0300

    Add UnitOfWork

---
+++ ProcessMaker_Data/UoW/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using ProcessMaker_Data.Context;
+using ProcessMaker_Data.Repositories.ProcessRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessMaker_Data.UoW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ProcessMakerDbContext context;
+
+        public IProcessRepository Processes { get; private set; }
+
+        public UnitOfWork(ProcessMakerDbContext context)
+        {
+            this.context = context;
+
+            Processes = new ProcessRepository(context);
+        }
+
+        public int Complete()
+        {
+            return context.SaveChanges();
+        }
+    }
+}
